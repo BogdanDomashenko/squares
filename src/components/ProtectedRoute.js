f51252cde4bf7c48a6c871d572568455680ddeb3
BogdanDomashenko <got.bogdan@gmail.com>
2022-04-03 18:59:20 +0300

    upd

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
+import axios from "axios";
+import config from "../config";
 
 function ProtectedRoute({ element, isLogined }) {
-  const userToken = localStorage.getItem("userToken");
+  const storageToken = localStorage.getItem("userToken") || " ";
+  const [userToken, setUserToken] = useState(null);
+
+  useEffect(() => {
+    isLogined &&
+      axios
+        .post(config.api + "/token", {
+          token: storageToken,
+        })
+        .then(({ data, headers }) => {
+          localStorage.setItem("userToken", headers.authorization);
+          setUserToken(headers.authorization);
+        })
+        .catch((error) => {
+          switch (error.toJSON().status) {
+            case 401:
+              localStorage.removeItem("userToken");
+              setUserToken(null);
+              break;
+            default:
+              console.error(error);
+              break;
+          }
+        });
+  });
 
   if (isLogined) {
     return userToken ? element : <Navigate to="/login" replace />;
